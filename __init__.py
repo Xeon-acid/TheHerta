fbbe9ed7ae10548eacaee9e232661209ec091140
NicoMico <Administrator@DESKTOP>
2025-08-18 07:44:13 +0800

    1

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ bl_info = {
     "name": "TheHerta",
     "description": "TheHerta",
     "blender": (3, 6, 0),
-    "version": (1, 9, 7),
+    "version": (1, 9, 8),
     "location": "View3D",
     "category": "Generic"
 }
