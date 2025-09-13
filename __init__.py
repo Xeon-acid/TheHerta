c4ad8ec6948916b0e9ec64549da73ffaf65f8340
NicoMico <Administrator@DESKTOP>
2025-09-14 03:46:50 +0800

    优化代码结构

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ bl_info = {
     "name": "TheHerta",
     "description": "TheHerta",
     "blender": (3, 6, 0),
-    "version": (2, 1, 2),
+    "version": (2, 1, 3),
     "location": "View3D",
     "category": "Generic"
 }
